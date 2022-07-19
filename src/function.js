67e5de9474ca666e4cb5b6a7715df5f55e076472
kurniadikevin <kurniadikevin@gmail.com>
2022-07-19 16:27:46 +0700

    set up webpack barebone

---
+++ src/function.js
@@ -0,0 +1,3 @@
+export const testImport = function(){
+    console.log('testing');
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+import { testImport } from "./function";
+
+testImport();
+
+console.log('hello world22222');
+console.log('test watch');
